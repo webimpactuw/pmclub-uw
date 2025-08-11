d71a0c8d02392daa042ebdb81ff6045804d23bff
teena1313 <tinalai.34@gmail.com>
2025-08-11 13:27:52 -0700

    hashrouter fix?

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ScrollToTop from "./scrollToTop";
 function App() {
   return (
     <div className="App"> 
-      <HashRouter basename="/pmclub-uw/#">
+      <HashRouter>
         <ScrollToTop />
         <Navbar />  
         <Routes>
